2022c541bcd23a7335ac4abe888cf40497a549de
MirandaKhoury <123335735+MirandaKhoury@users.noreply.github.com>
2024-04-11 13:39:54 -0400

    Update make_test_train_split.py

--- scripts/make_test_train_split.py
+++ scripts/make_test_train_split.py
@@ -1,14 +1,47 @@
-from keras import layers
-from tensorflow import data as tf_data
+#### make test train split ####
 
-image_size = (180, 180)
-batch_size = 128
+### first make a df containing all jpeg file names and their classes
+import os
+import numpy as np
 
-train_ds, val_ds = keras.utils.image_dataset_from_directory(
-    "path/to/folder/containing/2/subfolders/Fractured/and/Nonfractured",
-    validation_split=0.2,
-    subset="both",
-    seed=1337,
-    image_size=image_size,
-    batch_size=batch_size,
-)
+Frac_Atlas_path = "C://Users/mrkho/OneDrive/Documents/4th year/DS-4002/" # change to the directory where FracAtlas is
+
+frac = pd.DataFrame(os.listdir(Frac_Atlas_path + "FracAtlas/images/Fractured"))
+frac_name = pd.DataFrame(np.repeat("Fractured", 717))
+frac_df = pd.concat([frac, frac_name], axis=1, ignore_index=True)
+
+nonfrac = pd.DataFrame(os.listdir(Frac_Atlas_path + "FracAtlas/images/Non_fractured"))
+nonfrac_name = pd.DataFrame(np.repeat("Non-fractured", 3366))
+nonfrac_df = pd.concat([nonfrac, nonfrac_name], axis=1, ignore_index=True)
+
+df = pd.concat([frac_df, nonfrac_df], axis = 0 , ignore_index=True)
+
+### split into X and y
+X = df[0]
+y = df[1]
+
+### run test_train_split on X and y
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.30, shuffle=True,random_state = 0)
+
+X_train = pd.DataFrame(X_train)
+
+### use lists of jpegs to sort all images into appropriate folders
+
+# first set up directories:
+# in FracAtlas, make a new directory called "Split", then make 2 directories inside "Split" named "Test" and "Train"
+# copy all images from both "Fractured" and "Non-fractured" in "images" to "Split"
+
+# now get a list of all the file names...
+Split_path = Frac_Atlas_path + "FracAtlas/Split"
+from os import listdir
+from os.path import isfile, join
+file_list = [f for f in listdir(Split_path) if isfile(join(Split_path, f))] # get JUST files, not the Test/Train directories
+
+# and check if they're in X_train or not
+for file in file_list:
+    if X_train[0].str.contains(file).any():
+        os.rename(Split_path+"/"+file, Split_path+"/Train/"+file)
+    else:
+        os.rename(Split_path+"/"+file, Split_path+"/Test/"+file)
+    # move to Train if they are or Test if they aren't
